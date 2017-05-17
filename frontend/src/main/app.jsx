e7fd591cd3a6442ee7b41390952b5767496e03f7
Leonardo Salema <leosalema@yahoo.com.br>
2017-05-17 15:48:14 -0300

    Configuring the frontend

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'module/font-awesome/css/font-awesoma.min.css'
+
+import React from 'react'
+
+export default props => (
+    <div className='container'>
+        <h1>Teste</h1>
+    </div>
+)
+
---
+++ frontend/webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack')
+const ExtractTextPplugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devserver: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [
+        new: ExtractTextPplugin('app.css')
+    ], 
+    module: {
+        leoaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPplugin.extract('style-loader', 'css-loader')
+        },{
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
+    }
+}
